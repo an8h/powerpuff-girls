13c19e6dfa3ffdbe0b6dd3279bc34260e2ece430
an8h <anthie10@hotmail.com>
2020-08-01 10:19:55 +0200

    set up redux store

---
+++ src/redux/DefaultState.js
@@ -0,0 +1,20 @@
+/*
+Set a default state for the app
+ */
+export default {
+  movie: {
+    data: [],
+    isLoading: false,
+    errors: [],
+  },
+  episodes: {
+    data: [],
+    isLoading: false,
+    errors: [],
+  },
+  episode: {
+    data: [],
+    isLoading: false,
+    errors: [],
+  },
+};
---
+++ src/redux/Store.js
@@ -0,0 +1,35 @@
+// @flow
+
+/* 
+Set up Redux Store
+*/
+
+import {
+  applyMiddleware,
+  bindActionCreators,
+  combineReducers,
+  compose,
+  createStore,
+} from "redux";
+
+import reduxThunk from "redux-thunk";
+import * as actionCreators from "./Actions";
+import DefaultState from "./DefaultState";
+import * as reducers from "./Reducers";
+
+const rootReducer = combineReducers({
+  ...reducers,
+});
+const enhancers = compose(
+  applyMiddleware(reduxThunk),
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : (f) => f
+);
+
+const Store = createStore(rootReducer, DefaultState, enhancers);
+export default Store;
+
+export function mapDispachToProps(dispatch: Function) {
+  return bindActionCreators(actionCreators, dispatch);
+}
